37c723708a341dbf819d595bd9a96201477e935e
NewJerkey <bryschneider1@gmail.com>
2023-02-11 14:30:20 -0500

    initial commit

---
+++ my-card/src/my-card.js
@@ -0,0 +1,150 @@
+/* eslint-disable vars-on-top */
+/* eslint-disable no-var */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-plusplus */
+/* eslint-disable prefer-const */
+/* eslint-disable no-unused-vars */
+import { LitElement, html, css } from 'lit';
+
+// const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
+
+class MyCard extends LitElement {
+  static properties = {
+    header: { type: String },
+    cardCount: { type: Int16Array },
+  };
+
+  static styles = css`
+    body {
+      background: grey;
+    }
+    .card {
+      width: 300px;
+      text-align: center;
+      border: 3px solid yellow;
+      margin-top: 40px;
+      margin-bottom: 40px;
+      margin-right: 40px;
+      margin-left: 80px;
+      padding-bottom: 16px;
+      padding-right: 8px;
+      padding-left: 8px;
+      box-shadow: 3px 3px;
+      border-radius: 25px;
+      background: blue;
+    }
+
+    .paragraph {
+      font-size: 18px;
+      color: white;
+      margin-top: 24px;
+      margin-bottom: 24px;
+      margin-right: 24px;
+      margin-left: 24px;
+    }
+
+    .header {
+      font: 38px Papyrus;
+      color: white;
+      margin-bottom: 16px;
+    }
+
+    .buttons button {
+      border-radius: 25px;
+    }
+
+    .details {
+      border-radius: 25px;
+      color: yellow;
+      background-color: #4caf50;
+    }
+
+    button:hover,
+    button:focus {
+      background-color: yellow;
+    }
+
+    .image {
+      border: 3px solid yellow;
+      max-width: 250px;
+    }
+
+    @media (max-width: 800px) and (min-width: 500px) {
+      .button {
+        display: none;
+      }
+    }
+
+    @media (max-width: 500px) {
+      * {
+        transform: scale(0.8);
+      }
+    }
+  `;
+
+  constructor() {
+    super();
+    this.header = 'My app';
+    this.cardCount = 1;
+  }
+
+  render() {
+    return html`
+      <div class="buttons">
+        <button class="copy" @click=${this.copy}>Duplicate</button>
+        <button class="changeBackground" @click=${this.changeBG}>
+          Change Background
+        </button>
+        <button class="changeTitle" @click=${this.changeTitle}>
+          Change Title
+        </button>
+        <button id="delete" @click=${this.delete}>Delete Last Instance</button>
+      </div>
+      <div class="card">
+        <h2 class="header">Bryce Schneider</h2>
+        <img class="image" alt="Handsome Bryce"
+        src=https://cdn.discordapp.com/attachments/1062806966021402684/1063160172773654659/IMG_9420.png>
+        <p class="paragraph">
+          This image depicts a strapping young Penn State student reacting to an
+          insane moment in his favorite class: IST 256. He can not believe what
+          he is seeing!
+        </p>
+        <button class="details">Details</button>
+      </div>
+    `;
+  }
+
+  copy(event) {
+    const clone = this.shadowRoot.querySelector('.card').cloneNode(true);
+    this.shadowRoot.appendChild(clone);
+    this.cardCount++;
+  }
+
+  delete(event) {
+    if (this.cardCount > 1) {
+      this.shadowRoot.querySelector('.card:last-child').remove();
+      this.cardCount--;
+    }
+  }
+
+  changeTitle(event) {
+    if (
+      this.shadowRoot.querySelector('.header').innerHTML === 'Bryce Schneider'
+    ) {
+      this.shadowRoot.querySelector('.header').innerHTML = 'Something Else';
+    } else {
+      this.shadowRoot.querySelector('.header').innerHTML = 'Bryce Schneider';
+    }
+  }
+
+  changeBG(event) {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    this.shadowRoot.querySelector('.card').style.backgroundColor = color;
+  }
+}
+
+customElements.define('my-card', MyCard);
